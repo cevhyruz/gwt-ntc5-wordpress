ed178816f352952bbd27098e6626be42632098ec
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2024-09-15 10:52:24 +0800

    include my ongoing work

--- functions.php
+++ functions.php
@@ -2,4 +2,16 @@
 
 require get_stylesheet_directory() . '/inc/ntc-widget.php';
 
+function gwt_ntc_mock1_enqueue_scripts() {
+  $js_dir = get_stylesheet_directory() . '/js/';
+  $js_files = glob($js_dir . '*.js');
+
+  foreach ($js_files as $js_file) {
+    $file_name = basename($js_file);
+    $handle = 'custom-js-' . pathinfo($file_name, PATHINFO_FILENAME);
+    wp_enqueue_script($handle, get_stylesheet_directory_uri() . '/js/' . $file_name, array(), null, true);
+  }
+}
+add_action('wp_enqueue_scripts', 'gwt_ntc_mock1_enqueue_scripts');
+
 ?>
--- inc/ntc-widget.php
+++ inc/ntc-widget.php
@@ -3,24 +3,27 @@
  * ntc_wp default widgets
  *
  * @package GWT-NTC5
- * @since Government Website Template 2.0
+ * @since Government Website Template 26
  */
 
 class ntc_widget_apps extends WP_Widget {
   function __construct() {
     parent::__construct(
       'app_grid_buttons',
-      __('My Custom Widget', 'text_domain'),
-      array('description' => __('A Custom Widget for my child theme', 'text_domain')) // Args
+      __('App Widget', 'text_domain'),
+      array('description' => __('4x4 grid widget', 'text_domain')) // Args
     );
   }
 
   public function widget( $args, $instance ) {
-    // Widget output 
 		echo $args['before_widget'];
-    echo '<table class="widget_wrapper">
+    echo '
+<center>
+  <h2>Widgets Section</h2>
+</center>
+<table class="widget_wrapper">
     <tr class="widget_content" >
-      <td><img src="http://localhost/wordpress/wp-content/uploads/2024/09/GAD-Corner-Icon.png" alt="Image 1"></td>
+      <td><img src="uploads/2024/09/GAD-Corner-Icon.png" alt="Image 1"></td>
       <td><img src="http://localhost/wordpress/wp-content/uploads/2024/09/Icon-Forms.png" alt="Image 2"></td>
       <td><img src="http://localhost/wordpress/wp-content/uploads/2024/09/Icon-Gallery.png" alt="Image 3"></td>
       <td><img src="http://localhost/wordpress/wp-content/uploads/2024/09/Icon-Org.png" alt="Image 4"></td>
@@ -60,19 +63,22 @@ class ntc_widget_citizens_charter extends WP_Widget {
       parent::__construct(
           'citizens_charter', // Base ID
           __('Citizens Charter', 'text_domain'), // Name
-          array('description' => __('A Custom Widget for my child theme', 'text_domain')) // Args
+          array('description' => __('Citizen\'s Charter', 'text_domain')) // Args
       );
   }
   public function widget( $args, $instance ) {
     echo $args['before_widget'];
-    echo '<div class="container">
-    <div class="buttons">
-      <button class="button"><h2>Citizens Charter</h2></button>
-      <button class="button"><h2>Internal Services<h2></button>
-      <button class="button"><h2>External Services</h2></button>
-      <button class="button"><h2>Pursuant to RA No. 11032</h2></button>
+    echo '
+<center>
+  <h2>Citizens Charter</h2>
+</center>
+    <div class="btn-container">
+      <a id="buttons" href="#">INTERNAL SERVICES</a>
+      <a id="buttons" href="#">EXTERNAL SERVICES</a>
+      <a id="buttons" href="#">PURSUANT TO RA No. 11032</a>
     </div>
-  </div>';
+<div class="container">
+</div>';
     echo $args['after_widget'];
   }
 }
@@ -82,3 +88,165 @@ function citizens_charter_register_widgets() {
 }
 
 add_action( 'widgets_init', 'citizens_charter_register_widgets' );
+
+class ntc_widget_official_seals extends WP_Widget {
+  function __construct() {
+    parent::__construct(
+      'official_seals', // Base ID
+      __('Official Seals', 'text_domain'), // Name
+      array('description' => __('Official Government Seals', 'text_domain')) // Args
+    );
+  }
+
+  public function widget( $args, $instance ) {
+    echo $args['before_widget'];
+    echo '
+<center>
+ <h2>Official Seals</h2>
+</center>
+<div class="official-seals">
+  <img src="http://localhost/wordpress/wp-content/uploads/2024/09/official_ntc_logo_two-1.png" alt="Image 2">
+  <img src="http://localhost/wordpress/wp-content/uploads/2024/09/transparency-seal-160x160-1.png" alt="Image 1">
+  <img src="http://localhost/wordpress/wp-content/uploads/2024/09/foi-logo.png" alt="Image 3">
+</div>';
+    echo $args['after_widget'];
+  }
+}
+
+function official_seals_register_widget() {
+  register_widget( 'ntc_widget_official_seals' );
+}
+add_action( 'widgets_init', 'official_seals_register_widget' );
+
+class ntc_frontline_services extends WP_Widget {
+  function __construct() {
+    parent::__construct(
+      'frontline_services',
+      __('Frontline Services', 'text_domain'),
+      array('description' => __('Frontline Services', 'text_domain')) // Args
+    );
+  }
+
+  public function widget( $args, $instance ) {
+    echo $args['before_widget'];
+    echo '
+<center>
+  <h2>Frontline Services</h2>
+</center>
+
+<div id="frontline-services">
+    <div class="service-item">
+        <i class="fa fa-address-card" aria-hidden="true"></i>
+        <p>Licensing</p>
+    </div>
+
+    <div class="service-item">
+        <i class="fa fa-desktop" aria-hidden="true"></i>
+        <p>Monitoring</p>
+    </div>
+
+    <div class="service-item">
+        <i class="fa fa-binoculars" aria-hidden="true"></i>
+        <p>Inspection</p>
+    </div>
+</div>
+
+<div class="fs_container">
+    <img src="http://localhost/wordpress/wp-content/uploads/2024/09/png-transparent-gender-and-development-symbol-gender-miscellaneous-woman-technology.png" alt="Gender and Development">
+    <p>Gender and Development</p>
+  </div>
+
+  <div class="fs_container">
+    <img src="http://localhost/wordpress/wp-content/uploads/2024/09/315503733_2454779598002357_9211910661321317600_n-374x400-1.jpg" alt="Gender and Development">
+    <p>Consumer Welfare and Protection<p>
+  </div>
+
+';
+    echo $args['after_widget'];
+  }
+}
+
+function frontline_services_register_widgets() {
+  register_widget('ntc_frontline_services');
+}
+
+add_action('widgets_init', 'frontline_services_register_widgets');
+
+// Gender and development
+
+class ntc_gender_and_development extends WP_Widget {
+  function __construct() {
+    parent::__construct(
+      'gender_and_development',
+      __('Gender and Development', 'text_domain'),
+      array('description' => __('Gender and Development', 'text_domain')) // Args
+    );
+  }
+
+  public function widget( $args, $instance ) {
+    echo $args['before_widget'];
+    echo '
+  <img src="http://localhost/wordpress/wp-content/uploads/2024/09/png-clipart-logo-gender-and-development-brand-design-product-gender-and-development-logo-text-logo.png" alt="Gender and Development">
+    ';
+    echo $args['after_widget'];
+  }
+}
+
+function gender_and_development_register_widgets() {
+  register_widget( 'ntc_gender_and_development' );
+}
+
+add_action( 'widgets_init', 'gender_and_development_register_widgets' );
+
+class ntc5_site_gmap extends WP_Widget {
+  function __construct() {
+    parent::__construct(
+      'ntc5_site_gmap',
+      __('NTC5 Site GoogleMap', 'text_domain'),
+      array('description' => __('NTC Regional Office No. 5 googlemap location', 'text_domain')) // Args
+    );
+  }
+
+  public function widget( $args, $instance ) {
+    echo $args['before_widget'];
+    echo '<iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d476.70178245238924!2d123.75351267029981!3d13.166839810522037!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a101a0f3fe6445%3A0x17e72296faa8a00a!2sNATIONAL%20TELECOMMUNICATIONS%20COMMISSION!5e0!3m2!1sen!2sph!4v1726191752613!5m2!1sen!2sph" style="border:0; frameborder=0" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+';
+    echo $args['after_widget'];
+  }
+}
+
+function site_gmap_register_widgets() {
+  register_widget('ntc5_site_gmap');
+}
+add_action( 'widgets_init', 'site_gmap_register_widgets' );
+
+// affiliated websites
+
+
+class ntc5_affiliated_websites extends WP_Widget {
+  function __construct() {
+    parent::__construct(
+      'ntc5_affiliated_websites',
+      __('NTC Affiliated Websites', 'text_domain'),
+      array('description' => __('NTC affiliated Websites', 'text_domain'))
+    );
+  }
+  public function widget($args, $instance) {
+    echo $args['before_widget'];
+    echo '
+<center>
+  <h2>Affiliated Websites </h2>
+<div>
+      <img src="http://localhost/wordpress/wp-content/uploads/2024/09/dost_logo.png" alt="dost logo"></img>
+      <img src="http://localhost/wordpress/wp-content/uploads/2024/09/dict_logo.png" alt="dict logo"></img>
+</div>
+</center>
+';
+    echo $args['after_widget'];
+  }
+}
+
+function affiliated_websites_register_widgets() {
+  register_widget('ntc5_affiliated_websites');
+}
+add_action('widgets_init', 'affiliated_websites_register_widgets');
