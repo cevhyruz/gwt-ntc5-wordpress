a6cc39ee3a6c79c5770d9217062afcfe7b9ef24d
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2024-09-24 16:17:02 +0800

    Separate directors-corner to it's own widget

--- widgets/about_us.php
+++ widgets/about_us.php
@@ -1,10 +1,7 @@
 <?php
-
+  $regional_director = get_stylesheet_directory_uri() . "/images/RD SAM.png"
 ?>
 <h3>Welcome to NTC V</h3>
 
 <p> In this website, we aim to deliver effective and efficient
  public service to our clientele and relevant stakeholders in the region.</p>
-
-&nbsp;
-<h3><a href="#" class="link" >Organizational Chart</a><h3>
---
+++ widgets/directors_corner.php
@@ -0,0 +1,20 @@
+<?php
+$regional_director = get_stylesheet_directory_uri() . "/images/RD SAM.png";
+$orgchart = get_stylesheet_directory_uri(). "/images/orgchart_names_only.png";
+?>
+
+<div id="directors-corner">
+
+<div id="diretors-corner-wrapper">
+  <h3>Director's Corner</h3>
+  <img src="<?php echo $regional_director;?>">
+  <a class="link" href="<?php echo $orgchart; ?>" rel="lightbox">
+  View Organizational Chart
+  </a>
+</div>
+
+<p>ENGR. Samuel S. Sabile <br>
+<strong>Regional Director</strong>
+</p>
+
+</div>
