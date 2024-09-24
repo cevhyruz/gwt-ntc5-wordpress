16b21a41efcda49efbee75f5688bcf4c048d27f1
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2024-09-24 16:11:06 +0800

    Separate consumer welfare and protection to its own widget

---
+++ widgets/consumer_welfare.php
@@ -0,0 +1,10 @@
+<?php
+ $cwp = get_stylesheet_directory_uri() . "/images/consumer.jpg";
+?>
+
+<div class="consumer-container">
+<a href="tel:1682">
+  <img id="consumer" src="<?php echo $cwp; ?>" alt="Consumer Welfare and Protection">
+<h3>Consumer Welfare and Protection</h3>
+</a>
+</div>
