17be0db9ce68912cbf458be2344a1673135efb8b
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2024-10-11 10:49:33 +0800

    add links to point to their respective agency

--- widgets/affiliated_websites.php
+++ widgets/affiliated_websites.php
@@ -4,6 +4,11 @@ $dict_logo = get_stylesheet_directory_uri() . "/images/dost_logo_70x70.png"; ?>
 
   <div class="text-center">
     <h3>Affiliated Websites</h3>
-    <img src="<?php echo $dost_logo ?>" alt="dost logo">
-    <img src="<?php echo $dict_logo ?>" alt="dict logo">
-  </div>
+<a href="https://asti.dost.gov.ph/" target="_blank">
+    <img title="Department of Science and Technology" src="<?php echo $dost_logo ?>" alt="dost logo">
+</a>
+
+<a href="https://www.dict.gov.ph/" target="_blank">
+    <img title="Department of Information and Communications Technology" src="<?php echo $dict_logo ?>" alt="dict logo">
+</a>
+</div>
