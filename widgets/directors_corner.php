4fc2ef336e09daefd90cfac4b4ba7501a9d08481
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2024-10-11 16:25:37 +0800

    Centered elements

--- widgets/directors_corner.php
+++ widgets/directors_corner.php
@@ -5,14 +5,18 @@ $orgchart = get_stylesheet_directory_uri(). "/images/orgchart_names_only.png";
 
 <div id="directors-corner">
   <div id="directors-corner-wrapper">
+<center>
     <h3>
 <i class="fa fa-tag" aria-hidden="true"></i>
 Director's Corner</h3>
    <!-- <?php echo do_shortcode('[iheu_ultimate_oxi  id="7"]'); ?> -->
     <img src="<?php echo $regional_director;?>" rel="lightbox">
+</center>
   </div>
 </div>
 
+<center>
 <a class="link" href="<?php echo $orgchart; ?>">
-  <h3>View Organizational Chart</h3>
+  <center><h3>View Organizational Chart</h3></center>
 </a>
+</center>
