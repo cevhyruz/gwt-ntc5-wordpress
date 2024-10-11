86b4ef273a34a7fd6c0cb83f5b0eb8da393b47ef
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2024-10-11 10:38:09 +0800

    Fix broken links

--- widgets/citizens_charter.php
+++ widgets/citizens_charter.php
@@ -6,16 +6,16 @@
   $pursuant = get_stylesheet_directory_uri() . "/images/Pursuant to RA No. 11032.pdf";
 ?>
 
+<div class="citizens-charter">
+ <center>
 <h2>
 <i class="fa fa-bookmark" aria-hidden="true"></i>
 Citizens Charter
 <i class="fa fa-bookmark" aria-hidden="true"></i>
 </h2>
-<div class="citizens-charter">
- <center>
-  <h4><a title="internal services">internal services</a></h4>
-  <h4><a>external services</a></h4>
-  <h4><a>pursuant to ra no. 11032</a></h4>
+<h4><a href="<?php echo $internal;?>" target="_blank" title="Internal Services">internal services</a></h4>
+<h4><a href="<?php echo $external;?>" target="_blank" title="Enternal Services">external services</a></h4>
+<h4><a href="<?php echo $pursuant;?>" target="_blank" title="Pursuant to RA No. 11032">pursuant to ra no. 11032</a></h4>
 </center>
 
 <center>
